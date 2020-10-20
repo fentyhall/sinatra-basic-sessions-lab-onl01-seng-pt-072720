635dbcbb4aaab4bacea3a8637752075e54165703
fentyhall <fentymhall@gmail.com>
2020-10-19 18:54:03 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do 
+        erb :index 
+    end 
+
+    configure do
+        enable :sessions 
+    end 
+
+    post '/checkout' do 
+        session["item"] = params[:item]
+        @session = session
+    end 
 end
